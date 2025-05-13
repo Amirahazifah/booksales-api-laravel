57b8ec5afa16f7664e41db5a585f2bd99a849e83
Amirah Azifah Ayati <113626717+Amirahazifah@users.noreply.github.com>
2025-05-13 07:08:00 +0700

    Update AuthorController.php

--- booksales-api/booksales-api/app/Http/Controllers/AuthorController.php
+++ booksales-api/booksales-api/app/Http/Controllers/AuthorController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\AuthorModel; // Pastikan sudah mengimpor model AuthorModel
+use App\Models\AuthorModel;  
 
 class AuthorController extends Controller
 {
     public function index(){
-        $data1 = new AuthorModel(); // Buat instance dari AuthorModel
-        $authors = $data1->getAllAuthors();  // Gunakan metode yang sesuai di model
+        $data1 = new AuthorModel(); 
+        $authors = $data1->getAllAuthors();  
         
-        return view('authors', ['authors' => $authors]); // Kirim data ke view
+        return view('authors', ['authors' => $authors]); 
     }
 }
