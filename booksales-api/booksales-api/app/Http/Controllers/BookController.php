acf99dd97dd02a42f2d4bdedcebeb804bf84c4fc
Amirah Azifah Ayati <113626717+Amirahazifah@users.noreply.github.com>
2025-05-13 07:09:40 +0700

    Update BookController.php

--- booksales-api/booksales-api/app/Http/Controllers/BookController.php
+++ booksales-api/booksales-api/app/Http/Controllers/BookController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     public function index(){
-        $data = new BookModel(); // Perbaikan di sini
+        $data = new BookModel(); 
         $books = $data->getBooks();
         
         return view('books', ['books' => $books]);
